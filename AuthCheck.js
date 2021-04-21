f1cfbbd8902a5022181c6923f0b7fd4a0170def6
prathamaeron <65649322+prathamaeron@users.noreply.github.com>
2021-04-21 16:21:05 +0530

    Set up Auth with Firebase (iOS)

--- AuthCheck.js
+++ AuthCheck.js
@@ -4,24 +4,27 @@ import {
     Text,
     StyleSheet
 } from "react-native";
-
+import auth from "@react-native-firebase/auth"
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import auth from "@react-native-firebase/auth"
-
-
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import Login from "./screens/login"
+
 import Calculate from "./screens/calculate"
 import Results from "./screens/results"
 import Info from "./screens/results/info"
-import Working from "./screens/working"
+import Login from "./screens/login"
 import Loading from "./screens/loading"
+// import Contact from "./screens/contact"
+import Working from "./screens/working"
+
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+
+
 
 function ResultsStack ({route}) {
     return(
@@ -33,82 +36,83 @@ function ResultsStack ({route}) {
   }
 
 const AuthCheck = (props) => {
-    const [initializing, setInitializing] = useState(true);
-    const [user, setUser] = useState();
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-    function onAuthStateChanged(user) {
-        setUser(user);
-        if (initializing) setInitializing(false);
-    }
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
 
-    useEffect(() => {
-        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-        return subscriber; // unsubscribe on unmount
-    }, []);
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
 
-    if (initializing){
-        return(
-        <Loading />
-        )
-    };
+  if (initializing){
+    return(
+    <Loading />
+    )
+  };
 
-    if (!user) {
-        return (
-            <>
-        <NavigationContainer>
-        <Stack.Navigator headerMode = "none">
-        <Stack.Screen name = "Login" component={Login}/>
-        </Stack.Navigator>
-    </NavigationContainer>
-    </>
-        )
-    } else{
-        return(
-            <>
-        <StatusBar style="auto"/>
-        <NavigationContainer>
-        <Tab.Navigator
-        screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-    
-            if (route.name === 'Calculate') {
-                iconName = focused
-                ? 'calculator'
-                : 'calculator-outline';
-            } else if (route.name === 'Results') {
-                iconName = focused ? 'list' : 'list-outline';
-            } else if (route.name === 'Contact') {
-                iconName = focused ? 'information-circle' : 'information-circle-outline';
-            } else if (route.name === 'Working') {
-                iconName = focused ? 'book' : 'book-outline';
-            }
-    
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-            },
-        })}
-        tabBarOptions={{
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
-        }}
-        >
-            <Tab.Screen name="Calculate" component={Calculate} initialParams = {{user: user}}/>
-            <Tab.Screen name="Results" component={ResultsStack} initialParams = {{user: user}}/>
-            <Tab.Screen name="Working" component={Working} initialParams = {{user: user}}/>
-            {/* <Tab.Screen name="Contact" component={Contact} initialParams = {{user: user}}/> */}
-        </Tab.Navigator>
-    </NavigationContainer>
-    </>
-        )
-    }
-}
-  export default AuthCheck;
+  if (!user) {
+    return (
+        <>
+    <StatusBar style="auto"/>
+    <NavigationContainer>
+    <Stack.Navigator headerMode = "none">
+      <Stack.Screen name = "Login" component={Login}/>
+    </Stack.Navigator>
+  </NavigationContainer>
+  </>
+    )
+  } else{
+    return(
+        <>
+    <StatusBar style="auto"/>
+    <NavigationContainer>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
   
-  const styles = StyleSheet.create({
-      container: {
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center'
-      }
-  });
+          if (route.name === 'Calculate') {
+            iconName = focused
+              ? 'calculator'
+              : 'calculator-outline';
+          } else if (route.name === 'Results') {
+            iconName = focused ? 'list' : 'list-outline';
+          } else if (route.name === 'Contact') {
+            iconName = focused ? 'information-circle' : 'information-circle-outline';
+          } else if (route.name === 'Working') {
+            iconName = focused ? 'book' : 'book-outline';
+          }
+  
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+      >
+        <Tab.Screen name="Calculate" component={Calculate} initialParams = {{user: user}}/>
+        <Tab.Screen name="Results" component={ResultsStack} initialParams = {{user: user}}/>
+        <Tab.Screen name="Working" component={Working} initialParams = {{user: user}}/>
+        {/* <Tab.Screen name="Contact" component={Contact} initialParams = {{user: user}}/> */}
+      </Tab.Navigator>
+  </NavigationContainer>
+  </>
+    )
+  }
+}
+export default AuthCheck;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
--- screens/login/index.js
+++ screens/login/index.js
@@ -5,9 +5,33 @@ import {
     StyleSheet
 } from "react-native";
 
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import auth from "@react-native-firebase/auth"
+
+
+GoogleSignin.configure({
+  webClientId: '144144737879-8gq2g4ugvrpp572t4522d0d20k8lc02m.apps.googleusercontent.com',
+});
+
+const onGoogleButtonPress = async () => {
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+  
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
 const Login = (props) => (
     <View style={styles.container}>
-        <Text>Login</Text>
+    <GoogleSigninButton
+    style={{ width: 192, height: 48 }}
+    size={GoogleSigninButton.Size.Wide}
+    color={GoogleSigninButton.Color.Dark}
+    onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+    />
     </View>
     )
 export default Login;
